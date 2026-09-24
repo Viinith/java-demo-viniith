fbeb1b3d38ce990bc6b87ca51d0273b0a78f6aa5
Viniith <vinith200195@gmail.com>
2026-09-25 03:27:22 +0530

    Cleared the errors in the code.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public world HelloWorld {
-	public static void main(String [] args) {
-		System.out.print("Welcome to Edureka, DevOps online Training!");
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Edureka, DevOps online Training!");
 	}
 }
